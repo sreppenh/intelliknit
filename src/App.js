16774ba28b6bb188fa438c27c4f53c4b790fbe50
Stephanie Williams <stephaniewilliams@MacBookPro.wowway.com>
2025-07-29 19:27:46 -0400

    removing bounciness

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import IntelliknitMVP from './features/projects/components/IntelliknitMVP';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
       <IntelliknitMVP />
     </div>
   );
