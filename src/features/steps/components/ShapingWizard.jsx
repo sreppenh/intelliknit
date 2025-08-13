de5dad085ee9b72606e7f6a96c18733ad7394949
Stephanie Williams <stephaniewilliams@MacBookPro.wowway.com>
2025-08-13 13:37:38 -0400

    quick fix

--- src/features/steps/components/ShapingWizard.jsx
+++ src/features/steps/components/ShapingWizard.jsx
@@ -8,7 +8,7 @@ import IntelliKnitLogger from '../../../shared/utils/ConsoleLogging';
 import UnsavedChangesModal from '../../../shared/components/UnsavedChangesModal';
 import WizardContextBar from './wizard-layout/WizardContextBar';
 import PageHeader from '../../../shared/components/PageHeader';
-import SequentialPhasesWizard from './shaping-wizard/SequentialPhasesWizard';
+import SequentialPhasesWizard from './shaping-wizard/xx  SequentialPhasesWizard';
 
 const ShapingWizard = ({ wizardData, updateWizardData, currentStitches, construction, onBack,
   setConstruction, setCurrentStitches, component, componentIndex, onExitToComponentSteps, onGoToLanding, editingStepIndex = null }) => {
